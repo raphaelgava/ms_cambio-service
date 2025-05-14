de7f27d1599421270fc3221a60d19d5a98c0a921
Raphael Gava <raphaelgava1@gmail.com>
2025-05-14 15:01:48 -0300

    Adding files

---
+++ src/test/java/com/cambio_service/MsCambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
